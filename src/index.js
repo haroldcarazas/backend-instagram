587ab55291834cf5d0f8f1f9c952883dd58bd6cb
harold2001 <hcarazasnires@gmail.com>
2024-09-05 16:00:02 -0500

    configuración de cors terminada

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { PORT } from './config/config.js'
 import morgan from 'morgan'
 import userRoutes from './routes/users.routes.js'
 import authRoutes from './routes/auth.routes.js'
+import { validateCORS } from './middlewares/middleware.js'
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(validateCORS)
 app.use('/api/users', userRoutes)
 app.use('/api/auth', authRoutes)
 
